889a03a22dd504f284bc42b52f823885f057efe3
Adrian Kuhn <akuhn@iam.unibe.ch>
2012-12-13 02:10:28 -0800

    Bibtex of 2011.

--- download.rb
+++ download.rb
@@ -2,13 +2,15 @@ require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 
-id = 2337223
+id = 1806799
 
 url = "http://dl.acm.org/tab_about.cfm?id=#{id}&type=proceeding&parent_id=#{id}&parent_type=proceeding"
 doc = Nokogiri::HTML(open(url))
 ids = doc.search('a[href^="citation.cfm"]').collect{|m|m['href'][/id=(\d+)/,1]}
-ids.drop(1).each do |each|
+ids.each do |each|
   url = "http://dl.acm.org/exportformats.cfm?id=#{each}&expformat=bibtex"
   puts Nokogiri::HTML(open(url)).at('pre').content
-  `sleep 0.1`
+  `sleep #{rand}`
 end
+
+__END__
